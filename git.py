e675f2bc912a9772269c3f21de645ce380769836
Zachary Vance <za3k@za3k.com>
2016-06-21 21:55:29 -0700

    User fcgiwrap owns deadtree:/var/www/za3k_blog

--- git.py
+++ git.py
@@ -6,33 +6,41 @@ from fabric.api import cd, run, env, sudo, put, get, cd, settings, hosts
 from fabric.contrib import files
 from cuisine import dir_ensure, select_package, package_ensure
 
+def runorsuch(*args, **kwargs):
+    user = kwargs.get("user", None)
+    if user is None:
+        del kwargs["user"]
+        run(*args, **kwargs)
+    else:
+        sudo(*args, **kwargs)
+
 def ensure_git():
     select_package("apt")
     already_installed = package_ensure(["git"])
 
-def ensure_clone(remote, target):
+def ensure_clone(remote, target, user=None):
     if files.exists(target):
         with cd(target):
-            run('git pull')
+            runorsuch('git pull', user=user)
     else:
-        clone(remote, target)
+        clone(remote, target, user=user)
 
-def ensure_clone_github(repository, target, use_https=True):
+def ensure_clone_github(repository, target, user=None, use_https=True):
     if use_https:
         remote="https://github.com/{repository}.git".format(repository=repository)
     else:
         remote="git@github.com:{repository}.git".format(repository=repository)
-    ensure_clone(remote, target)
+    ensure_clone(remote, target, user=user)
 
-def ensure_clone_za3k(repository, target, use_https=True):
+def ensure_clone_za3k(repository, target, user=None, use_https=True):
     if use_https:
         remote="https://git.za3k.com/{repository}.git".format(repository=repository)
     else:
         remote="deadtree.xen.prgmr.com:/git/{repository}.git".format(repository=repository)
-    ensure_clone(remote, target)
+    ensure_clone(remote, target, user=user)
 
-def clone(remote, target):
-    run('git clone "{remote}" "{target}"'.format(remote=remote, target=target))
+def clone(remote, target, user=None):
+    runorsuch('git clone "{remote}" "{target}"'.format(remote=remote, target=target), user=user)
 
 def canonical_location(name):
     dir_ensure("/source", mode='1755')
