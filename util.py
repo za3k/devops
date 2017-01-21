7d8c71d67197f00c9e02cd94dcc1a916e2154688
Zachary Vance <za3k@za3k.com>
2017-01-21 13:38:42 -0800

    util.put supports uploading files and not just folders

--- util.py
+++ util.py
@@ -2,5 +2,5 @@ import fabric.api
 
 def put(source, destination, user, **kwargs):
     ret = fabric.api.put(source, destination, use_sudo=True, **kwargs)
-    fabric.api.sudo('chown -R {user}:{user} {folder}'.format(user=user, folder=destination + "/" + source.split("/")[-1]))
+    fabric.api.sudo('[ -f {file} ] && chown {user}:{user} {file} || chown -R {user}:{user} {folder}'.format(user=user, folder=destination + "/" + source.split("/")[-1], file=destination))
     return ret
