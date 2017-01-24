4a84af93e877952ce905a5b364a1d76067b90e1b
Zachary Vance <za3k@za3k.com>
2017-01-23 23:55:39 -0800

    Add cross-signining for Let's Encrypt cert

--- letsencrypt.py
+++ letsencrypt.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from fabric.api import run, env, sudo, put, get, cd, settings, hosts
 from cuisine import dir_ensure, group_ensure, group_user_ensure, user_ensure
 import crypto
-import path, git
+import path, git, util
 
 well_known_base = '/var/www/well-known'
 
@@ -21,6 +21,7 @@ def ensure():
             sudo("./acme.sh --install", user='acme')
     dir_ensure(well_known_base)
     sudo("chown acme:acme {well_known} && chmod 755 {well_known}".format(well_known=well_known_base)) # Can't change attributes without sudo in a sticky (not write) directory... annoying
+    util.put("config/certs/lets-encrypt-x3-cross-signed.pem", "/etc/ssl/certs", user='root', mode='0644')
 
 def add_csr(path, domain):
     well_known = well_known_base + '/' + domain
@@ -29,5 +30,6 @@ def add_csr(path, domain):
         with settings(warn_only=True):
             sudo(".acme.sh/acme.sh --signcsr --csr {path} -w {well_known}".format(path=path, well_known=well_known), user='acme')
         sudo("chown acme:acme /etc/ssl/certs/{domain}.pem".format(domain=domain))
-        sudo(".acme.sh/acme.sh --installcert -d {domain} --certpath /etc/ssl/certs/{domain}.pem".format(path=path, domain=domain), user='acme')
+        sudo(".acme.sh/acme.sh --installcert -d {domain} --certpath /etc/ssl/certs/{domain}.letsencrypt.pem".format(path=path, domain=domain), user='acme')
+        sudo("cat /etc/ssl/certs/{domain}.letsencrypt.pem /etc/ssl/certs/lets-encrypt-x3-cross-signed.pem >/etc/ssl/certs/{domain}.pem".format(path=path, domain=domain), user='acme')
     return '/etc/ssl/certs/{domain}.key'.format(domain=domain)
